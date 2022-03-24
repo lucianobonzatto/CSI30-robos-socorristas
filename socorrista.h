5edfd28eaf04335b64f84f5f30d69cd408041cf4
lucianobonzatto <37905647+lucianobonzatto@users.noreply.github.com>
2022-03-24 14:09:23 -0300

    update socorrista

--- socorrista.h
+++ socorrista.h
@@ -3,8 +3,9 @@
 //
 
 #ifndef TAREFA_1_SOCORRISTA_H
-#define TAREFA_1_SOCORRISTA_H
+#include "includes.h";
 
+#define TAREFA_1_SOCORRISTA_H
 
 class socorrista {
 private:
@@ -13,17 +14,32 @@ private:
     int numPacotes;
     int tempoRestante;
     int* mapa;
-
+    vector<float*> victimsV;
+    
     //constantes
     int tamAmbiente[2];
+    
+    //metodos
+    void initMat();
+    
 public:
     socorrista();
+    ~socorrista();
+    
+    int moveDecision();
+    void moveResult(int result, const int newPose[2], float time);
+    
     void setMapSize(int mapSize[2]);
     void setBat(int bat);
     void setTime(int time);
     void setNumPacotes(int num);
-
+    
+    void includeMap(int* pMap);
+    void includeVictim(float* victim);
+    
     void getPose(int* poseReturn);
+    void printMap();
+    void printVictims();
 };
 
 #endif //TAREFA_1_SOCORRISTA_H
